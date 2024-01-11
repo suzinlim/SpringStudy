f525387429fd04f23ee64c693950d37a8b964e90
suzinlim <122861956+suzinlim@users.noreply.github.com>
2024-01-11 23:26:08 +0900

    feat: 스프링 데이터 JPA

--- 1-spring-introduction/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ 1-spring-introduction/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -12,15 +12,22 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
-//    private DataSource dataSource;
+    //    private EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
 
+//    private DataSource dataSource;
+//
 //    @Autowired
 //    public SpringConfig(DataSource dataSource) {
 //        this.dataSource = dataSource;
@@ -28,14 +35,14 @@ public class SpringConfig {
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRespository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
 }
