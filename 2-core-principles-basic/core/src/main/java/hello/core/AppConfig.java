80446e9264f58841dd01943bbdb98105c61e35d3
suzinlim <122861956+suzinlim@users.noreply.github.com>
2024-01-20 19:23:15 +0900

    feat: 스프링으로 전환

--- 2-core-principles-basic/core/src/main/java/hello/core/AppConfig.java
+++ 2-core-principles-basic/core/src/main/java/hello/core/AppConfig.java
@@ -8,23 +8,30 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // 애플리케이션에 대한 환경 구성
 // 실제 동작에 필요한 구현 객체를 생성
+@Configuration // 설정을 구성한다는 뜻
 public class AppConfig {
 
+    @Bean // 스프링 컨테이너에 등록
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
